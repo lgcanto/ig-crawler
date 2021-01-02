3164bbf405d7410fa57f9c6fa1bb10017787e3e0
Lucas Canto <lucas.canto@siemens.com>
2021-01-01 23:31:20 -0300

    moving ig username and pass to env file

--- dev/src/main.py
+++ dev/src/main.py
@@ -1,29 +1,31 @@
-from selenium import webdriver
+import os
 import time
+from selenium import webdriver
+
+IG_USERNAME = str(os.getenv("IG_USERNAME", ""))
+IG_PASSWORD = str(os.getenv("IG_PASSWORD", ""))
 
 options = webdriver.FirefoxOptions()
 options.headless = True
 driver = webdriver.Firefox(options=options)
 driver.get('https://www.instagram.com/accounts/login/')
-time.sleep(5) # wait until everything loads
+time.sleep(2) # wait until everything loads
 dom = driver.find_element_by_xpath('//*')
 
-# pdb.set_trace()
 username = dom.find_element_by_name('username')
 password = dom.find_element_by_name('password')
 login_button = dom.find_element_by_xpath('//*[@class="sqdOP  L3NKy   y3zKF     "]')
 
 username.clear()
 password.clear()
-username.send_keys('your username')
-password.send_keys('your password')
+username.send_keys(IG_USERNAME)
+password.send_keys(IG_PASSWORD)
 
 login_button.click()
-# driver.get('https://www.instagram.com/accounts/login')
 
-time.sleep(5) # wait until everything loads
-print("html page after trying login:")
-print(driver.page_source)
+time.sleep(2) # wait until everything loads
 
-if 'logged-in' in driver.page_source:
+if 'not-logged-in' not in driver.page_source and 'logged-in' in driver.page_source:
     print('Logged in')
+else:
+    print('Login failed')
