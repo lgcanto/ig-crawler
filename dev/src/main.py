a9aba34a154722e2bf0f058dee9a6abea58e322c
Lucas Canto <lucas.canto@siemens.com>
2021-01-03 02:57:39 -0300

    script ready, printing goal info. Still need to make the code better and maintainable

--- dev/src/main.py
+++ dev/src/main.py
@@ -29,3 +29,51 @@ if 'not-logged-in' not in driver.page_source and 'logged-in' in driver.page_sour
     print('Logged in')
 else:
     print('Login failed')
+
+driver.get('https://www.instagram.com/' + IG_USERNAME + '/')
+time.sleep(2) # wait until everything loads
+driver.find_element_by_partial_link_text('followers').click()
+
+previous_scroll_position = 0
+scroll_position = 1
+while(previous_scroll_position != scroll_position):
+    previous_scroll_position = scroll_position
+    scroll_position = driver.execute_script('var followersPanelElement = document.getElementsByClassName("isgrP")[0];followersPanelElement.scrollTop = followersPanelElement.scrollHeight; return followersPanelElement.scrollTop;')
+    print("scrolling followers...")
+    time.sleep(3)
+# print("finished scrolling")
+
+followers_elements = driver.find_elements_by_xpath('//*[@class="FPmhX notranslate  _0imsa "]')
+followers =  list(map(lambda element: element.text, followers_elements))
+
+driver.get('https://www.instagram.com/' + IG_USERNAME + '/')
+time.sleep(2) # wait until everything loads
+driver.find_element_by_partial_link_text('following').click()
+
+previous_scroll_position = 0
+scroll_position = 1
+while(previous_scroll_position != scroll_position):
+    previous_scroll_position = scroll_position
+    scroll_position = driver.execute_script('var followersPanelElement = document.getElementsByClassName("isgrP")[0];followersPanelElement.scrollTop = followersPanelElement.scrollHeight; return followersPanelElement.scrollTop;')
+    print("scrolling following...")
+    time.sleep(3)
+# print("finished scrolling")
+
+followings_elements = driver.find_elements_by_xpath('//*[@class="FPmhX notranslate  _0imsa "]')
+followings =  list(map(lambda element: element.text, followings_elements))
+
+print('you follow, but they do not follow you:')
+not_following_you = []
+for following in followings:
+    if following not in followers:
+        not_following_you.append(following)
+for account in not_following_you:
+    print(account)
+
+print('they follow you, but you dont follow them back:')
+you_dont_follow = []
+for follower in followers:
+    if follower not in followings:
+        you_dont_follow.append(follower)
+for account in you_dont_follow:
+    print(account)
